2e7125aac44cad78a076f2ad7873006be92b2a9e
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-01-28 02:59:25 +1100

    Add scraper for council

--- scraper.rb
+++ scraper.rb
@@ -1,40 +1,128 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-
-Bundler.require
-
 require "scraperwiki"
 require "mechanize"
 
 class Scraper
-  def self.run
+  INITIAL_PAGE_URL = "https://www.clarence.nsw.gov.au/Building-and-planning/Development-applications/Advertised-DAs"
+  STATE = "NSW"
+
+  def clean_whitespace(text)
+    text.gsub("\r", " ").gsub("\n", " ").squeeze(" ").strip
+  end
+
+  attr_accessor :pause_duration
+
+  def throttle_block(extra_delay: 0.5)
+    if @pause_duration
+      puts "  Pausing #{@pause_duration}s"
+      sleep(@pause_duration)
+    end
+    start_time = Time.now.to_f
+    page = yield
+    @pause_duration = (Time.now.to_f - start_time + extra_delay).round(3)
+    page
+  end
+
+  def cleanup_old_records
+    cutoff_date = (Date.today - 30).to_s
+    vacuum_cutoff_date = (Date.today - 35).to_s
+
+    stats = ScraperWiki.sqliteexecute(
+      "SELECT COUNT(*) as count, MIN(date_scraped) as oldest FROM data WHERE date_scraped < ?",
+      [cutoff_date]
+    ).first
+
+    deleted_count = stats["count"]
+    oldest_date = stats["oldest"]
+
+    return unless deleted_count.positive? || ENV["VACUUM"]
+
+    puts "Deleting #{deleted_count} applications scraped between #{oldest_date} and #{cutoff_date}"
+    ScraperWiki.sqliteexecute("DELETE FROM data WHERE date_scraped < ?", [cutoff_date])
+
+    return unless rand < 0.03 || (oldest_date && oldest_date < vacuum_cutoff_date) || ENV["VACUUM"]
+
+    puts "  Running VACUUM to reclaim space..."
+    ScraperWiki.sqliteexecute("VACUUM")
+  end
+
+  def run
     agent = Mechanize.new
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    # Read in a page
-    page = agent.get("https://example.com")
-
-    # Find something on the page using css selectors
-    page.search("h1").each do |h1|
-      value = h1.text.strip
-      # Write out to the sqlite database using scraperwiki library
-      ScraperWiki.save_sqlite(["name"], { "name" => value })
+    page = throttle_block do
+      puts "Getting advertised DAs page"
+      agent.get(INITIAL_PAGE_URL)
     end
 
-    # An arbitrary query against the database
-    rows = ScraperWiki.select("rowid AS id, name FROM data ORDER BY rowid desc LIMIT 3")
-    rows.each do |row|
-      puts "#{row['id']}: #{row['name']}"
+    # Find all DA articles
+    container = page.at("div.da-list-container")
+    raise "Could not find DA list container" unless container
+
+    articles = container.search("article")
+    added = found = 0
+
+    articles.each do |article|
+      found += 1
+
+      link = article.at("a")
+      next unless link
+
+      info_url = link["href"]
+      council_reference = article.at("p.da-application-number")&.text&.strip
+      address = article.at("p.list-item-address")&.text&.strip
+      address = "#{address}, #{STATE}" if address.to_s != "" && !address.end_with?(STATE)
+
+      # Description is the first <p> without a class
+      description = link.search("p").find { |p| !p["class"] }&.text&.strip
+
+      unless council_reference
+        puts "Warning: No council reference found! (skipped)"
+        next
+      end
+      unless address
+        puts "Warning: No address found! (skipped)"
+        next
+      end
+      unless description
+        puts "Warning: No description found! (skipped)"
+        next
+      end
+
+      record = {
+        "council_reference" => council_reference,
+        "address" => address,
+        "description" => clean_whitespace(description),
+        "info_url" => info_url,
+        "date_scraped" => Date.today.to_s,
+      }
+
+      added += 1
+      puts "Saving record #{council_reference} - #{address}"
+      ScraperWiki.save_sqlite(["council_reference"], record)
     end
+
+    cleanup_old_records
+    puts "Added #{added} records, and skipped #{found - added} unprocessable records."
+
+    # Check pagination
+    pagination = page.at("div.seamless-pagination-info")
+    if pagination
+      pagination_text = clean_whitespace(pagination.text)
+      puts "Found pagination: #{pagination_text}"
+
+      unless pagination_text == "Page 1 of 1"
+        warn "ERROR: Multiple pages detected but pagination not implemented!"
+        exit 1
+      end
+    else
+      warn "ERROR: Unable to detect if further pages exist!"
+      exit 2
+    end
+    puts "Finished!"
   end
 end
 
-# Run the scraper whilst allowing this file to be required in tests without auto-execution
-Scraper.run if __FILE__ == $PROGRAM_NAME
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+Scraper.new.run if __FILE__ == $PROGRAM_NAME
